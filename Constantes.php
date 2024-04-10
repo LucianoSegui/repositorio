e4e21b18b302da2f0a32268fa5bfbd9fd174247e
luciano <luchoaramis@gmail.com>
2024-04-10 08:53:49 -0300

    ActConstantes

---
+++ Constantes.php
@@ -0,0 +1,20 @@
+<!-- 
+    1) git config --global user.name "luciano" 
+
+     2) git config --global user.email "luchoaramis@gmail.com"
+
+     3) git clone https://github.com/LucianoSegui/repositorio.git
+-->
+
+<!-- Otra manera de escribirlo 
+    const pi = 3.1416; -->
+
+<html>
+    <head>Definir </head>
+    <body>
+    
+    <?php define ('PI', 3.14.); ?>
+    
+    <?php echo PI; ?>
+    </body>
+</html>
