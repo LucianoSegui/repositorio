77cd8da19a43a1b076cbc98bdc47b4e11e17d755
luciano <luchoaramis@gmail.com>
2024-04-10 09:14:32 -0300

    printVar

---
+++ print.php
@@ -0,0 +1,17 @@
+<html>
+    <head> </head>
+    <body>
+    
+    <?php  $nombre = "Luciano";  ?>
+    <?php  $edad = 21;  ?>
+    <?php  $estatura= 1.85;  ?>
+
+    <!-- var_dump muestra el tipo de variable, el contenido y la letra -->
+    <?php var_dump($nombre) ?>
+    <?php var_dump($edad) ?>
+    <?php var_dump($estatura) ?>
+
+    <!-- muestra solo el contenido de la variable -->
+    <?php print_r($nombre) ?>
+    </body>
+</html>
