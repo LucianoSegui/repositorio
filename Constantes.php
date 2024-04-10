bf6084a4051f2d47d9ca3a9c52e32bac9c702715
luciano <luchoaramis@gmail.com>
2024-04-10 08:59:47 -0300

    ActConst

--- Constantes.php
+++ Constantes.php
@@ -1,9 +1,17 @@
 <!-- 
+    -Levantar servidor apache y poner la carpeta en htdocs
+
     1) git config --global user.name "luciano" 
 
      2) git config --global user.email "luchoaramis@gmail.com"
 
      3) git clone https://github.com/LucianoSegui/repositorio.git
+
+     4) git add (nombre constante)
+
+     5) git git commit -m "texto corto"
+
+     6) git push
 -->
 
 <!-- Otra manera de escribirlo 
